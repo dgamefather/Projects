c72a50e5fa605b59a8be5fd551b10171370a0de7
dgamefather <dgamefather@thatoneplace.net>
2019-12-16 14:47:34 -0700

    Updated props

--- nightwatch.props.js
+++ nightwatch.props.js
@@ -1,10 +1,10 @@
 module.exports = {
-    // PC
+    // PC \\
     resourcePath: "..\\testing-resources\\",
     seleniumServer: "selenium-server-standalone-3.141.59.jar",
     chromedriver: "chromedriver.exe",
 
-    // // Mac
+    // Mac \\
     // resourcePath: "../testing-resources/",
     // seleniumServer: "selenium-server-standalone-3.141.59.jar",
     // chromedriver: "chromedriver"
