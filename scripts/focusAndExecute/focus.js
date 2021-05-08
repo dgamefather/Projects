f68199cfce7e5351408cb4ce741a7aea0d664a51
Brennan Litster <brennan@thatoneplace.net>
2021-05-08 17:01:49 -0600

    Forgot to uncomment
    All the sct cases should've been not commented out when the last commit happened.

--- scripts/focusAndExecute/focus.js
+++ scripts/focusAndExecute/focus.js
@@ -126,20 +126,20 @@ module.exports = {
 
     'Score Check': browser => {     // WORKS!
         scorecard
-            // .override()
+            .override()
             .sct001()   // This scenario WILL FAIL! Won't show on scoreboard as of 1/27/2021
-			// .sct002()
-            // .sct003()
-            // .sct004()
-            // .sct005()
-            // .sct006()
-            // .sct007()
-            // .sct008()
-            // .sct009()
-            // .sct010()
-            // .sct011()
-            // .sct012()
-            // .sct013();
+			.sct002()
+            .sct003()
+            .sct004()
+            .sct005()
+            .sct006()
+            .sct007()
+            .sct008()
+            .sct009()
+            .sct010()
+            .sct011()
+            .sct012()
+            .sct013();
     },
 
     // /*** Green Light Review ***/
